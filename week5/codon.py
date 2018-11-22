2727db58600ddb15b506cd9438a4312eeab48e03
neilswainston <neil.swainston@manchester.ac.uk>
2018-11-22 17:46:39 +0000

    Updated Python code for Week 5.

--- week5/codon.py
+++ week5/codon.py
@@ -1,28 +1,41 @@
-seq = 'ATGCGACGAT'
+seqs = []
 
-# Get number of codons using integer division:
-num_of_codons = len(seq) // 3
-print(num_of_codons)
+seqs_file = open('seqs.txt')
 
-# Initialise an empty dictionary with which to count codons:
-codon_count = {}
+for line in seqs_file:
+    seqs.append(line.strip())
 
-# Generate range / "list" of codon start indexes:
-codon_indexes = range(0, num_of_codons * 3, 3)
-print(list(codon_indexes))
+seqs_file.close()
 
-# Loop through the codon indexes, and use the 'slice' notation to extract
-# codons from the sequence:
-for codon_index in codon_indexes:
-    print(codon_index)
-    codon = seq[codon_index:codon_index + 3]
-    print(codon)
+print(seqs)
 
-    # How do we count these with our empty dictionary?
-    # Hint: take a look at the 'in' operator on Slide 7 of Week 4's slides.
-    if codon in codon_count:
-        codon_count[codon] += 1
-    else:
-        codon_count[codon] = 1
+for seq in seqs:
+    # Get number of codons using integer division:
+    num_of_codons = len(seq) // 3
+    print(num_of_codons)
 
-    print(codon_count)
+    # Initialise an empty dictionary with which to count codons:
+    codon_count = {}
+
+    # Generate range / "list" of codon start indexes:
+    codon_indexes = range(0, num_of_codons * 3, 3)
+    print(list(codon_indexes))
+
+    # Loop through the codon indexes, and use the 'slice' notation to extract
+    # codons from the sequence:
+    for codon_index in codon_indexes:
+        print(codon_index)
+        codon = seq[codon_index:codon_index + 3]
+        print(codon)
+        
+        if 'N' in codon:
+            continue
+
+        # How do we count these with our empty dictionary?
+        # Hint: take a look at the 'in' operator on Slide 7 of Week 4's slides.
+        if codon in codon_count:
+            codon_count[codon] += 1
+        else:
+            codon_count[codon] = 1
+
+        print(codon_count)
---
+++ week5/my_funcs.py
@@ -0,0 +1,10 @@
+print(len('Hello'))
+
+def my_len():
+    count = 0
+
+    return count
+
+print(my_len())
+
+
