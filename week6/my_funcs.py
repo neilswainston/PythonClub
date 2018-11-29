b7cf3bfff9833694f5f484e45e3c9df45c52f158
neilswainston <neil.swainston@manchester.ac.uk>
2018-11-29 11:14:39 +0000

    Updated PythonClub code.

---
+++ week6/my_funcs.py
@@ -0,0 +1,7 @@
+def my_len(lst):
+    count = 0
+
+    for item in lst:
+        count += 1
+
+    return count
---
+++ week6/test.py
@@ -0,0 +1,4 @@
+import my_funcs
+
+print(len('hello'))
+print(my_funcs.my_len('hello'))
