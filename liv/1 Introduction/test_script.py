e7bf1ac83a71c9e67de825eb4d95a9d091bc36e7
Neil Swainston <neil.swainston@liverpool.ac.uk>
2020-11-20 01:02:33 +0000

    Interim check-in: needs checking.

--- liv/1 Introduction/test_script.py
+++ liv/1 Introduction/test_script.py
@@ -1,1 +1,10 @@
-print('5 squared =', 5**2)
+# Simple test script.
+
+# I'm a comment, by the way. I'm human-readable stuff that Python ignores.
+# But, I'm very useful for documenting and adding hints to my code.
+# Anything that you write after a hashtag is a comment.
+
+print('Hello, Python Club!')
+print('5 squared is...')
+print(5**2)
+6**2
